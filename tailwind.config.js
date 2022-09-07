e09d59c131717d40b2c522caa9f9c445af22f04f
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-09-07 17:59:48 +0300

    feat: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,21 +15,14 @@ module.exports = {
       inherit: colors.inherit,
       current: colors.current,
       transparent: colors.transparent,
-      // TODO: Add colors
-      // Make sure that they are prepared in the Figma and follow the naming primary/secondary/gray-${number}
-      // Example of correctly prepared colors in Figma — https://user-images.githubusercontent.com/20713191/143586876-5e834233-9639-4166-9811-b00e63820d98.png
-      // Example of incorrectly prepared colors in Figma — https://user-images.githubusercontent.com/20713191/143586974-6986149f-aee3-450c-a1dd-26e73e3aca02.png
-      // black: '',
-      // white: '',
-      // primary: {
-      //   1: '',
-      // },
-      // secondary: {
-      //   1: '',
-      // },
-      // gray: {
-      //   1: '',
-      // },
+      black: '#000000',
+      white: '#ffffff',
+      primary: {
+        1: '#FF0095',
+      },
+      gray: {
+        1: '#C7CCD1',
+      },
     }),
     screens: {
       '2xl': { max: '1919px' },
