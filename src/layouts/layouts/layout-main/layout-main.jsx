5bdc3f2b2ab2141a8e5e416c23f7481887ef7109
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-09-08 21:30:05 +0300

    chore: add small adjustments

--- src/layouts/layouts/layout-main/layout-main.jsx
+++ src/layouts/layouts/layout-main/layout-main.jsx
@@ -1,10 +1,17 @@
+import PropTypes from 'prop-types';
+
 import Seo from 'components/shared/seo';
 
 const LayoutMain = ({ children }) => (
   <>
     <Seo />
+
     <main>{children}</main>
   </>
 );
 
 export default LayoutMain;
+
+LayoutMain.propTypes = {
+  children: PropTypes.node.isRequired,
+};
