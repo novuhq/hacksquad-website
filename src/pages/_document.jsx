77bf757409034dcf8fd751a85f26fb63170990cd
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-09-12 19:13:07 +0300

    chore: preload fonts

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,10 +1,30 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
+const fontsBasePath = '/fonts';
+
+const fontsPaths = [
+  '/bugfast/bugfast-regular.woff2',
+  '/input-mono/input-mono-regular.woff2',
+  '/flood-std/flood-std-italic.woff2',
+  '/roboto-mono/roboto-mono-medium.woff2',
+];
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {fontsPaths.map((fontPath, index) => (
+            <link
+              rel="preload"
+              href={`${fontsBasePath}${fontPath}`}
+              as="font"
+              type="font/woff2"
+              crossOrigin="anonymous"
+              key={index}
+            />
+          ))}
+        </Head>
         <body>
           <Main />
           <NextScript />
