9bd72d076622a863d7074d8f2d43191dd3b77d5a
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-09-12 19:52:53 +0300

    feat: add load state for buttons

--- src/components/shared/button/button.jsx
+++ src/components/shared/button/button.jsx
@@ -1,26 +1,40 @@
 import clsx from 'clsx';
 import { signIn } from 'next-auth/react';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 
 import ArrowIcon from 'icons/arrow.inline.svg';
 
-const Button = ({ className }) => (
-  <button
-    className={clsx(
-      'button relative inline-flex h-9 items-center justify-center whitespace-nowrap px-4 text-center !leading-none outline-none before:absolute before:inset-0 before:-z-10 before:border-2 after:absolute after:inset-0 after:-z-10',
-      className
-    )}
-    type="button"
-    onClick={(e) => {
-      e.preventDefault();
-      signIn('github', { callbackUrl: '/thank-you/' });
-    }}
-  >
-    <span>Join now</span>
-    <ArrowIcon className="ml-2.5 block h-3" aria-hidden />
-  </button>
-);
+const Button = ({ className }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    signIn('github', { callbackUrl: '/thank-you/' });
+  };
+
+  return (
+    <button
+      className={clsx(
+        'button relative inline-flex h-9 items-center justify-center whitespace-nowrap px-4 text-center !leading-none outline-none before:absolute before:inset-0 before:-z-10 before:border-2 after:absolute after:inset-0 after:-z-10',
+        className,
+        isLoading && 'pointer-events-none min-w-[135px]'
+      )}
+      type="button"
+      onClick={handleSignIn}
+    >
+      {isLoading ? (
+        <div className="h-3.5 w-3.5 animate-spin rounded-full border border-b border-transparent border-b-white" />
+      ) : (
+        <>
+          <span>Join now</span>
+          <ArrowIcon className="ml-2.5 block h-3" aria-hidden />
+        </>
+      )}
+    </button>
+  );
+};
 
 Button.propTypes = {
   className: PropTypes.string,
--- src/components/shared/sign-up-button/sign-up-button.jsx
+++ src/components/shared/sign-up-button/sign-up-button.jsx
@@ -1,37 +1,51 @@
 import clsx from 'clsx';
 import { signIn } from 'next-auth/react';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 
 import GitHubIcon from 'icons/github.inline.svg';
 
-const SignUpButton = ({ className }) => (
-  <button
-    className={clsx(
-      'sign-up-btn relative flex h-[60px] max-w-full items-center justify-center !leading-none',
-      className
-    )}
-    type="button"
-    onClick={(e) => {
-      e.preventDefault();
-      signIn('github', { callbackUrl: '/thank-you/' });
-    }}
-  >
-    <svg
-      className="sign-up-btn-border xs:w-full"
-      width="341"
-      height="59"
-      viewBox="0 0 341 59"
-      fill="none"
+const SignUpButton = ({ className }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    signIn('github', { callbackUrl: '/thank-you/' });
+  };
+
+  return (
+    <button
+      className={clsx(
+        'sign-up-btn relative flex h-[60px] max-w-full items-center justify-center !leading-none',
+        className,
+        isLoading && 'pointer-events-none'
+      )}
+      type="button"
+      onClick={handleSignIn}
     >
-      <path d="M1 58V1H324.586L340 16.4142V58H1Z" stroke="white" strokeWidth="2" />
-    </svg>
-    <div className="absolute inset-0 flex items-center justify-center space-x-2.5">
-      <GitHubIcon className="h-[25px]" />
-      <span className="text-lg sm:text-[18px]">Sign up with GitHub</span>
-    </div>
-  </button>
-);
+      <svg
+        className="sign-up-btn-border xs:w-full"
+        width="341"
+        height="59"
+        viewBox="0 0 341 59"
+        fill="none"
+      >
+        <path d="M1 58V1H324.586L340 16.4142V58H1Z" stroke="white" strokeWidth="2" />
+      </svg>
+      <div className="absolute inset-0 flex items-center justify-center space-x-2.5">
+        {isLoading ? (
+          <div className="h-7 w-7 animate-spin rounded-full border border-b border-transparent border-b-white" />
+        ) : (
+          <>
+            <GitHubIcon className="h-[25px]" />
+            <span className="text-lg sm:text-[18px]">Sign up with GitHub</span>
+          </>
+        )}
+      </div>
+    </button>
+  );
+};
 
 SignUpButton.propTypes = {
   className: PropTypes.string,
