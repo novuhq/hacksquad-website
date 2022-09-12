9af7ac1b2908f400aa1be64fc652300b0d87e19c
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-09-12 17:11:12 +0300

    feat: set user email

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -12,5 +12,40 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  events: {
+    async signIn({ user, account }) {
+      // Get user email if we don't have it in public emails
+      if (!user.email) {
+        try {
+          // fetch user email
+          const emails = await fetch('https://api.github.com/user/emails', {
+            headers: {
+              Authorization: `token ${account.accessToken}`,
+            },
+          }).then((res) => res.json());
+
+          if (!emails || emails.length === 0) {
+            return;
+          }
+
+          // Sort by primary email - the user may have several emails, but only one of them will be primary
+          const sortedEmails = emails.sort((a, b) => b.primary - a.primary);
+          const primaryEmail = sortedEmails[0].email;
+
+          // update user email
+          await prisma.user.update({
+            where: {
+              id: user.id,
+            },
+            data: {
+              email: primaryEmail,
+            },
+          });
+        } catch (err) {
+          console.log('Failed to set email', err);
+        }
+      }
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
 });
