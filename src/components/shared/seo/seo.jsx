e6088cba615d59e7e542b249316b34c0c6bd746a
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-09-13 20:30:00 +0300

    feat: add favicon, social preview

--- src/components/shared/seo/seo.jsx
+++ src/components/shared/seo/seo.jsx
@@ -1,9 +1,21 @@
 import Head from 'next/head';
 
+const title = 'Hacksquad 2022 - Join Hacktoberfest with your squad';
+const description =
+  'Hacktoberfest: a month-long celebration of open-source projects, their maintainers, and the entire community of contributors.';
+
 const Seo = () => (
   <Head>
-    <title>Hacksquad 2022 - Join Hacktoberfest with your squad</title>
-    <link rel="icon" href="/favicon.ico" />
+    <title>{title}</title>
+    <link rel="icon" href="/favicon.png" type="image/png" />
+
+    {/* Open Graph */}
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content="/social-preview.jpg" />
+    <meta property="og:type" content="website" />
+    {/* Twitter */}
+    <meta name="twitter:card" content="summary_large_image" />
   </Head>
 );
 
