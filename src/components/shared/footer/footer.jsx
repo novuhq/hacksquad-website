cd5281d4fe8139bad3d234e51444aeefd7d452ce
Andrew Golovanov <spacewebdeveloper@gmail.com>
2022-09-14 14:36:37 +0300

    feat: update styles for the mobile footer version

--- src/components/shared/footer/footer.jsx
+++ src/components/shared/footer/footer.jsx
@@ -17,9 +17,9 @@ const Footer = () => (
         </Link>
         <Button className="hidden flex-shrink-0 sm:flex" />
       </div>
-      <div className="flex items-center space-x-10 sm:mt-6 sm:w-full sm:flex-col sm:justify-center">
-        <nav>
-          <ul className="flex space-x-10 md:space-x-6">
+      <div className="flex items-center space-x-10 sm:mt-6 sm:w-full">
+        <nav className="sm:w-full">
+          <ul className="flex space-x-10 md:space-x-6 sm:justify-between">
             {MENUS.header.map(({ href, text }, index) => (
               <li key={index}>
                 <Link href={href} passHref>
