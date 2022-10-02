16af9e3d3f4135ef6a1983abd5957ddd724b2e18
Nevo David <nevo@notu.co>
2022-10-02 13:15:04 +0700

    fix: faster leaderboard

--- src/pages/api/leaderboard.js
+++ src/pages/api/leaderboard.js
@@ -8,6 +8,13 @@ export default async function handler(req, res) {
     where: {
       disqualified: false,
     },
+    select: {
+      id: true,
+      name: true,
+      score: true,
+      slug: true,
+    },
   });
+
   res.status(200).json({ teams });
 }
