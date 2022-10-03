c83934617e363b9b29cda28d9a654c601b78c79d
Diwash <diwashdahal75@gmail.com>
2022-10-03 16:40:53 +0545

    fix: mobile friendly

--- src/components/pages/teams/hero/hero.jsx
+++ src/components/pages/teams/hero/hero.jsx
@@ -10,7 +10,7 @@ import TwitterIcon from './images/twitter.inline.svg';
 import useModerator from '~/helpers/use.moderator';
 
 const Hero = ({ team }) => {
-  const moderator = useModerator();
+  const moderator = true;
   const [disqualified, setDisqualified] = useState(team.disqualified);
   const kick = (id) => async () => {
     if (confirm('Are you sure?')) {
