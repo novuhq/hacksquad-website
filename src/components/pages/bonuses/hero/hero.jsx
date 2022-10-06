ba70be4394806a6d3bb91ba0fd3fe116c6eb0cb8
Nevo David <nevo@notu.co>
2022-10-06 14:28:32 +0700

    feat: typ

--- src/components/pages/bonuses/hero/hero.jsx
+++ src/components/pages/bonuses/hero/hero.jsx
@@ -26,7 +26,7 @@ const Hero = ({ twitter }) => (
           <ul>
             <li className="grid grid-cols-[230px_485px_230px_1fr] gap-x-5 border-b border-gray-2 py-4 lg:grid-cols-[130px_390px_1fr_1fr] md:grid-cols-[130px_485px_230px_1fr] sm:grid-cols-[70px_150px_230px_1fr]">
               <span>1</span>
-              <span>Connect twitter account to like and retweet HackSquad twitter</span>
+              <span>Connect your twitter account to like and retweet HackSquad twitter</span>
               <span>2</span>
               <span>{twitter ? 'Connected' : <TwitterButton />}</span>
             </li>
