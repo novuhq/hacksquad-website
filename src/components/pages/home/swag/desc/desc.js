748909cc1954123819fa9e49900518b0c683a7af
Seek4samurai <grvrawat999@gmail.com>
2022-10-07 18:20:15 +0530

    style: added swag item img urls

--- src/components/pages/home/swag/desc/desc.js
+++ src/components/pages/home/swag/desc/desc.js
@@ -2,34 +2,42 @@ const desc = [
   {
     id: '0',
     desc: 'Get the best fit for you and your team! Hacksquad T-shirts for all the coolest contributors.',
+    img: '/_next/static/media/t-shirt.fd9859a6.jpg',
   },
   {
     id: '1',
     desc: 'For the Coffee addicts, those who converts caffeine into code that is capable to impact the lives of the community.',
+    img: '/_next/static/media/mug.0f25d8fd.jpg',
   },
   {
     id: '2',
     desc: 'Stickers make your code run faster? These Hacksquad stickers will make sure it is true. Best for your workplace.',
+    img: '/_next/static/media/sticker.7771700c.jpg',
   },
   {
     id: '3',
     desc: 'Pretty clean bag with Hacksquad logo would be so nice, I guess this one looks super cool, with that logo.',
+    img: '/_next/static/media/bag.7a10dd52.jpg',
   },
   {
     id: '4',
     desc: 'Hacksquad logo is pretty clean in everywhere and looks super cool even on this box...',
+    img: '/_next/static/media/box.c5dd365c.jpg',
   },
   {
     id: '5',
     desc: 'If you need a hoodie for upcoming winters, this might be the best option for a cool Hacksquad member like you!',
+    img: '/_next/static/media/hoodie.15ff81e2.jpg',
   },
   {
     id: '6',
     desc: 'Get the best fit for you and your team! Hacksquad T-shirts for all the coolest contributors.',
+    img: '/_next/static/media/t-shirt-2.d394c493.jpg',
   },
   {
     id: '7',
     desc: 'Stickers make your code run faster? These Hacksquad stickers will make sure it is true. Best for your workplace.',
+    img: '/_next/static/media/sticker-2.a52f0672.jpg',
   },
 ];
 
