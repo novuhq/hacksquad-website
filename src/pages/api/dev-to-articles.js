08486aa8f05a661186cb2ce885eae3bf6fcce4bb
Nevo David <nevo@notu.co>
2022-10-24 20:16:36 +0700

    feat: articles

--- src/pages/api/dev-to-articles.js
+++ src/pages/api/dev-to-articles.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
   const articles = await prisma.articles.findMany({
     orderBy: {
-      createdAt: 'desc',
+      createdAt: 'asc',
     },
   });
 
