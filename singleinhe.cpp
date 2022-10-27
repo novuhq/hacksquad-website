b36fead7d225ade79c97765d31dc6c381904381c
Kishubhai <106054122+Kishubhai@users.noreply.github.com>
2022-10-27 22:03:31 +0530

    Create singleinhe.cpp

---
+++ singleinhe.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  
+using namespace std;  
+ class Account {  
+   public:  
+   float salary = 60000;   
+ };  
+   class Programmer: public Account {  
+   public:  
+   float bonus = 5000;    
+   };       
+int main(void) {  
+     Programmer p1;  
+     cout<<"Salary: "<<p1.salary<<endl;    
+     cout<<"Bonus: "<<p1.bonus<<endl;    
+    return 0;  
+}  
