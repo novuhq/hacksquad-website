2b0d9415806a81480488d22f6da61d0994dbd599
sohammesare <50767537+sohammesare@users.noreply.github.com>
2022-10-29 12:27:49 +0530

    Create TimeOfTimezone.py

---
+++ TimeOfTimezone.py
@@ -0,0 +1,7 @@
+from datetime import *
+import pytz
+  
+  
+tz_INDIA = pytz.timezone('Asia/Kolkata') 
+datetime_INDIA = datetime.now(tz_INDIA)
+print("INDIA time:", datetime_INDIA.strftime("%H:%M:%S"))
