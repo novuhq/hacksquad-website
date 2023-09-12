23138f2d9b4435d86786a5e963d8cff2de759967
Raman Yasel <raman.yasel@gmail.com>
2023-09-12 10:39:49 +0300

    chore: update seo component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,10 @@
+import getMetadata from 'lib/get-metadata';
+import { SEO_DATA } from 'lib/seo-data';
+
 const Home = () => <div />;
 
 export default Home;
+
+export async function generateMetadata() {
+  return getMetadata(SEO_DATA.INDEX);
+}
---
+++ src/lib/seo-data.js
@@ -0,0 +1,7 @@
+export const SEO_DATA = {
+  INDEX: {
+    title: 'HackSquad 2023 🚀',
+    description: 'Contribute code, meet community members, participate in workshops, and win SWAG!',
+    pathname: '',
+  },
+};
