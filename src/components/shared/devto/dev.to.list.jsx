59fc661cd77e3e0add209a8ad7e785d2c5cc0882
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-09-14 01:21:38 +0400

    fix: minor changes

--- src/components/shared/devto/dev.to.list.jsx
+++ src/components/shared/devto/dev.to.list.jsx
@@ -43,6 +43,7 @@ const DevToAccountList = () => {
 
   useEffect(() => {
     connectDevTo();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
--- src/context/session-provider.jsx
+++ src/context/session-provider.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
