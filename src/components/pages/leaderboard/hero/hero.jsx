d745861e9e3dee29b3e36b647333345b44a710f7
Raman Yasel <raman.yasel@gmail.com>
2023-09-13 16:29:05 +0300

    feat: add leaderboard coming soon

--- src/components/pages/leaderboard/hero/hero.jsx
+++ src/components/pages/leaderboard/hero/hero.jsx
@@ -1,15 +1,35 @@
+/* eslint-disable no-unused-vars */
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
 const tableGridClass = 'grid grid-cols-[145px_510px_1fr] gap-x-5 md:grid-cols-[100px_400px_1fr]';
 
 const Hero = ({ teams = [] }) => (
-  <section className="safe-paddings relative min-h-[600px] py-24 sm:py-20">
+  <section className="safe-paddings relative py-24 sm:py-20">
     <div className="container flex h-full flex-col items-center justify-center">
       <h1 className="max-w-2xl font-titles text-60 font-semibold leading-none md:text-42 xs:max-w-[246px]">
         Leaderboard
       </h1>
+
+      {/* TODO: Remove this block completely after leaderboard are published  */}
       <div className="md:scrollbar-hidden mx-auto mt-[76px] w-[798px] md:w-full md:overflow-x-auto">
+        <div className="container">
+          <div className="grid grid-cols-[145px_510px_1fr] gap-x-5 border-b border-white border-opacity-20 pb-4 md:grid-cols-[1fr_1fr_1fr]">
+            <span className="text-20 font-medium uppercase leading-normal md:text-18">Place</span>
+            <span className="text-20 font-medium uppercase leading-normal md:text-18">Name</span>
+            <span className="text-20 font-medium uppercase leading-normal md:text-18">Score</span>
+          </div>
+
+          <span className="coming-soon-animation block border-b border-white border-opacity-20 py-4 text-center">
+            Coming soon <span>.</span>
+            <span>.</span>
+            <span>.</span>
+          </span>
+        </div>
+      </div>
+
+      {/* TODO: Display this block after leaderboard are published */}
+      {/* <div className="md:scrollbar-hidden mx-auto mt-[76px] w-[798px] md:w-full md:overflow-x-auto">
         <div className="md:min-w-[700px] md:px-7 sm:px-4">
           <div className={clsx('border-b border-white border-opacity-20 pb-4', tableGridClass)}>
             <span className="text-20 font-medium uppercase leading-normal md:text-18">Place</span>
@@ -32,7 +52,7 @@ const Hero = ({ teams = [] }) => (
             ))}
           </ul>
         </div>
-      </div>
+      </div> */}
     </div>
   </section>
 );
