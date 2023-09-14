7c6898b7aad6085bc9c8019c8705ce7bf8503728
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-09-14 18:19:42 +0400

    chore: add small adjustments

--- src/app/(website)/invite/[id]/page.jsx
+++ src/app/(website)/invite/[id]/page.jsx
@@ -5,14 +5,14 @@ import { auth } from 'lib/auth';
 async function Invite({ params }) {
   const session = await auth();
 
+  if (!session?.user) {
+    return redirect('/sign-in');
+  }
+
   await fetch(`${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}/api/invite`, {
     method: 'POST',
     body: JSON.stringify({ id: params.id }),
   });
-
-  if (!session?.user) {
-    return redirect('/sign-in');
-  }
 
   return redirect('/my-team');
 }
