f041ae046afbcf0a575005586425106d6807f20a
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-09-15 02:25:49 +0400

    fix: minor changes

--- src/components/pages/home/hero/hero.jsx
+++ src/components/pages/home/hero/hero.jsx
@@ -9,9 +9,9 @@ import heroImage from './images/home-hero.png';
 import Sponsors from './sponsors';
 
 const Hero = ({ isAuthorized = false }) => (
-  <section className="hero safe-paddings relative overflow-hidden pb-5 pt-[373px] lg:pt-[260px] md:pt-[230px]">
+  <section className="hero safe-paddings relative overflow-hidden pb-5 pt-[373px] lg:pt-[260px] md:pt-[230px] sm:pt-[300px] xs:pt-[230px]">
     <div className="container relative z-20 sm:text-center">
-      <h1 className="max-w-2xl bg-white bg-home-hero-title bg-clip-text pb-6 font-titles text-114 font-semibold leading-none text-transparent lg:text-92 md:text-60 xxs:max-w-[246px] xxs:text-42">
+      <h1 className="max-w-2xl bg-white bg-home-hero-title bg-clip-text pb-6 font-titles text-114 font-semibold leading-none text-transparent lg:text-92 md:text-60 xxs:text-42">
         <span className="block text-80 leading-[72px]">2023</span>
         Hacksquad
       </h1>
@@ -31,7 +31,7 @@ const Hero = ({ isAuthorized = false }) => (
       <Sponsors />
     </div>
 
-    <div className="absolute left-0 top-0 h-full max-h-[761px] w-full overflow-hidden lg:max-h-[650px] md:max-h-[560px]">
+    <div className="absolute left-0 top-0 h-full max-h-[761px] w-full overflow-hidden lg:max-h-[650px] md:max-h-[560px] sm:max-h-[480px] xs:max-h-[310px]">
       <div
         className="absolute left-1/2 right-0 top-0 ml-[9.2%] h-full w-full min-w-[1920px] -translate-x-1/2"
         style={{
@@ -40,7 +40,7 @@ const Hero = ({ isAuthorized = false }) => (
         }}
       />
       <Image
-        className="absolute bottom-0 left-1/2 ml-[9.2%] max-w-[1568px] -translate-x-1/2 object-cover lg:max-w-[1288px] md:max-w-[1130px] sm:ml-0"
+        className="absolute bottom-0 left-1/2 ml-[9.2%] max-w-[1568px] -translate-x-1/2 object-cover lg:max-w-[1288px] md:max-w-[1130px] sm:ml-0 sm:max-w-[990px] xs:-ml-2.5 xs:max-w-[570px]"
         src={heroImage}
         sizes="100vw"
         alt=""
