4f9df699bb95ed36fbee3f323e82994ca44442fe
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-09-15 21:26:10 +0400

    chore: add small adjustments

--- src/app/(website)/page.jsx
+++ src/app/(website)/page.jsx
@@ -4,12 +4,13 @@ import QuestionAndAnswer from 'components/pages/home/question-and-answer';
 import TwitterTimeline from 'components/pages/home/twitter-timeline';
 import DynamicTicket from 'components/pages/ticket/dynamic-ticket';
 import { auth } from 'lib/auth';
+import findUser from 'lib/find-user';
 import getMetadata from 'lib/get-metadata';
 import { SEO_DATA } from 'lib/seo-data';
 
 async function HomePage() {
   let userData = {
-    id: '0000000001',
+    ticketId: '0000000001',
     name: 'Your.Name',
     handle: 'GitHub account',
     colorSchema: '1',
@@ -17,9 +18,11 @@ async function HomePage() {
   const session = await auth();
 
   if (session && session.user) {
+    const user = await findUser(session.user.userId);
+
     userData = {
       ...session.user,
-      id: session.user.userId,
+      ticketId: user.ticketId,
       handle: session.user.githubHandle,
     };
   }
