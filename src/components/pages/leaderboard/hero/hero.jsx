039453e6bd900870ecc0e9f22fb8e735859b93bb
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-09-15 13:59:50 +0400

    fix: get teams for leaderboard page

--- src/components/pages/leaderboard/hero/hero.jsx
+++ src/components/pages/leaderboard/hero/hero.jsx
@@ -5,54 +5,54 @@ import PropTypes from 'prop-types';
 const tableGridClass = 'grid grid-cols-[145px_510px_1fr] gap-x-5 md:grid-cols-[100px_400px_1fr]';
 
 const Hero = ({ teams = [] }) => (
-  <section className="safe-paddings relative py-24 sm:py-20">
+  <section className="safe-paddings relative pb-24 pt-40 sm:pb-20">
     <div className="container flex h-full flex-col items-center justify-center">
       <h1 className="max-w-2xl font-titles text-60 font-semibold leading-none md:text-42 xs:max-w-[246px]">
         Leaderboard
       </h1>
 
-      {/* TODO: Remove this block completely after leaderboard are published  */}
-      <div className="md:scrollbar-hidden mx-auto mt-[76px] w-[798px] md:w-full md:overflow-x-auto">
-        <div className="container">
-          <div className="grid grid-cols-[145px_510px_1fr] gap-x-5 border-b border-white border-opacity-20 pb-4 md:grid-cols-[1fr_1fr_1fr]">
-            <span className="text-20 font-medium uppercase leading-normal md:text-18">Place</span>
-            <span className="text-20 font-medium uppercase leading-normal md:text-18">Name</span>
-            <span className="text-20 font-medium uppercase leading-normal md:text-18">Score</span>
+      {teams.length === 0 ? (
+        <div className="md:scrollbar-hidden mx-auto mt-[76px] w-[798px] md:w-full md:overflow-x-auto">
+          <div className="container">
+            <div className="grid grid-cols-[145px_510px_1fr] gap-x-5 border-b border-white border-opacity-20 pb-4 md:grid-cols-[1fr_1fr_1fr]">
+              <span className="text-20 font-medium uppercase leading-normal md:text-18">Place</span>
+              <span className="text-20 font-medium uppercase leading-normal md:text-18">Name</span>
+              <span className="text-20 font-medium uppercase leading-normal md:text-18">Score</span>
+            </div>
+
+            <span className="coming-soon-animation block border-b border-white border-opacity-20 py-4 text-center">
+              Coming soon <span>.</span>
+              <span>.</span>
+              <span>.</span>
+            </span>
           </div>
-
-          <span className="coming-soon-animation block border-b border-white border-opacity-20 py-4 text-center">
-            Coming soon <span>.</span>
-            <span>.</span>
-            <span>.</span>
-          </span>
         </div>
-      </div>
+      ) : (
+        <div className="md:scrollbar-hidden mx-auto mt-[76px] w-[798px] md:w-full md:overflow-x-auto">
+          <div className="md:min-w-[700px] md:px-7 sm:px-4">
+            <div className={clsx('border-b border-white border-opacity-20 pb-4', tableGridClass)}>
+              <span className="text-20 font-medium uppercase leading-normal md:text-18">Place</span>
+              <span className="text-20 font-medium uppercase leading-normal md:text-18">Name</span>
+              <span className="text-20 font-medium uppercase leading-normal md:text-18">Score</span>
+            </div>
 
-      {/* TODO: Display this block after leaderboard are published */}
-      {/* <div className="md:scrollbar-hidden mx-auto mt-[76px] w-[798px] md:w-full md:overflow-x-auto">
-        <div className="md:min-w-[700px] md:px-7 sm:px-4">
-          <div className={clsx('border-b border-white border-opacity-20 pb-4', tableGridClass)}>
-            <span className="text-20 font-medium uppercase leading-normal md:text-18">Place</span>
-            <span className="text-20 font-medium uppercase leading-normal md:text-18">Name</span>
-            <span className="text-20 font-medium uppercase leading-normal md:text-18">Score</span>
+            <ul>
+              {teams.map((team, index) => (
+                <li
+                  className={clsx(
+                    'border-b border-white border-opacity-20 py-4 text-20 leading-normal md:text-18',
+                    tableGridClass
+                  )}
+                >
+                  <span className="text-gray-1">{index + 1}</span>
+                  <p>{team.name}</p>
+                  <span>{team.score}</span>
+                </li>
+              ))}
+            </ul>
           </div>
-
-          <ul>
-            {teams.map((team, index) => (
-              <li
-                className={clsx(
-                  'border-b border-white border-opacity-20 py-4 text-20 leading-normal md:text-18',
-                  tableGridClass
-                )}
-              >
-                <span className="text-gray-1">{index + 1}</span>
-                <p>{team.name}</p>
-                <span>{team.score}</span>
-              </li>
-            ))}
-          </ul>
         </div>
-      </div> */}
+      )}
     </div>
   </section>
 );
--- src/lib/get-teams.js
+++
@@ -1,6 +0,0 @@
-const getTeams = async () =>
-  fetch(`${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}/api/leaderboard`, {
-    next: { revalidate: 3600 },
-  }).then((response) => response.json());
-
-export default getTeams;
