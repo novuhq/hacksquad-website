6ad649ede7ece4eb07d719e178fd26df6feaca41
Andrew Golovanov <spacewebdeveloper@gmail.com>
2023-09-16 14:21:41 +0400

    test: revalidate leaderboard page

--- src/app/(website)/leaderboard/page.jsx
+++ src/app/(website)/leaderboard/page.jsx
@@ -1,15 +1,24 @@
-/* eslint-disable no-unused-vars */
 import Hero from 'components/pages/leaderboard/hero';
 import JoinUs from 'components/shared/join-us';
 import getMetadata from 'lib/get-metadata';
 import { SEO_DATA } from 'lib/seo-data';
 
-const LeaderboardPage = () => (
-  <>
-    <Hero />
-    <JoinUs />
-  </>
-);
+async function getTeams() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_DEFAULT_SITE_URL}/api/leaderboard`);
+  const data = await response.json();
+  return data;
+}
+
+async function LeaderboardPage() {
+  const teamsList = await getTeams();
+
+  return (
+    <>
+      <Hero teams={teamsList} />
+      <JoinUs />
+    </>
+  );
+}
 
 export async function generateMetadata() {
   return getMetadata(SEO_DATA.LEADERBOARD);
