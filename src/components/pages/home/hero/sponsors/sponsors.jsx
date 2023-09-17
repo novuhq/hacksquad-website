13d16723b7ac87c76676550b0105780a5039dd16
Nevo David <nevo@notu.co>
2023-09-17 12:59:16 +0700

    feat: link

--- src/components/pages/home/hero/sponsors/sponsors.jsx
+++ src/components/pages/home/hero/sponsors/sponsors.jsx
@@ -9,24 +9,28 @@ const logos = [
   {
     src: toolJet,
     alt: 'ToolJet',
+    link: 'https://tooljet.com',
     width: 119,
     height: 42,
   },
   {
     src: crowdDev,
     alt: 'CrowdDev',
+    link: 'https://www.crowd.dev',
     width: 149,
     height: 44,
   },
   {
     src: hanko,
     alt: 'Hanko',
+    link: 'https://www.hanko.io',
     width: 126,
     height: 44,
   },
   {
     src: wasp,
     alt: 'Wasp',
+    link: 'https://wasp-lang.dev',
     width: 121,
     height: 44,
   },
@@ -37,7 +41,10 @@ const Sponsors = () => (
     <ul className="flex flex-wrap justify-center gap-x-[111px] gap-y-10 md:gap-x-16 sm:gap-x-10">
       {logos.map((props, index) => (
         <li className="shrink-0" key={index}>
-          <Image className="w-auto sm:h-9" priority {...props} />
+          {/* eslint-disable-next-line react/prop-types */}
+          <a href={props.link} target="_blank" rel="noreferrer">
+            <Image className="w-auto sm:h-9" priority {...props} />
+          </a>
         </li>
       ))}
     </ul>
