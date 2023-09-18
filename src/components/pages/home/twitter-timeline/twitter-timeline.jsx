2b5b652574b3f1a17527fa06a795c34ec3b704b2
Raman Yasel <raman.yasel@gmail.com>
2023-09-18 13:38:16 +0300

    chore: update twitter-timeline content

--- src/components/pages/home/twitter-timeline/twitter-timeline.jsx
+++ src/components/pages/home/twitter-timeline/twitter-timeline.jsx
@@ -4,15 +4,28 @@ import Link from 'components/shared/link';
 
 import User1ContentPhoto from './images/user-1-content.jpg';
 import User1Photo from './images/user-1.jpg';
+import User10ContentPhoto from './images/user-10-content.jpg';
+import User10Photo from './images/user-10.jpg';
+import User11ContentPhoto from './images/user-11-content.jpg';
+import User11Photo from './images/user-11.jpg';
+import User12ContentPhoto from './images/user-12-content.jpg';
+import User12Photo from './images/user-12.jpg';
+import User2ContentPhoto from './images/user-2-content.jpg';
 import User2Photo from './images/user-2.jpg';
+import User3ContentPhoto from './images/user-3-content.jpg';
 import User3Photo from './images/user-3.jpg';
 import User4ContentPhoto from './images/user-4-content.jpg';
 import User4Photo from './images/user-4.jpg';
+import User5ContentPhoto from './images/user-5-content.jpg';
 import User5Photo from './images/user-5.jpg';
+import User6ContentPhoto from './images/user-6-content.jpg';
 import User6Photo from './images/user-6.jpg';
 import User7ContentPhoto from './images/user-7-content.jpg';
 import User7Photo from './images/user-7.jpg';
+import User8ContentPhoto from './images/user-8-content.jpg';
 import User8Photo from './images/user-8.jpg';
+import User9ContentPhoto from './images/user-9-content.jpg';
+import User9Photo from './images/user-9.jpg';
 
 const list = [
   {
@@ -36,18 +49,15 @@ const list = [
   },
   {
     twitterPhoto: User2Photo,
-    twitterName: 'Priyanshu',
-    twitterAccountName: 'Kumar_Sons_off',
+    twitterName: 'Shivangi Singh',
+    twitterAccountName: 'heyssr17',
     twitterUrl: 'https://twitter.com/Kumar_Sons_off/status/1647967956343873537',
     text: (
       <>
-        Bit late, but finally the HackSquad22 Swags arrived. 🎉
-        <br />
-        <br />A great thank to <span>@nevodavid</span> & the team behind for organising such a
-        friendly & pleasant competition.
-        <br />
-        <br />
-        Open-Source FTW! 🚀
+        Most awaited Swags of the year 2022 Hacksquad is here. <br /> <br /> Thanks to{' '}
+        <span className="contents">@nevodavid</span>, the Hacksquad 2022 challenge went really
+        smoothly. <br />
+        <br /> Also thanks to the sponsors
         <p className="text-with-hashtags">
           <span>@HackSquadDev</span>
           <span>@novuhq</span>
@@ -57,132 +67,44 @@ const list = [
         </p>
       </>
     ),
+    photo: User2ContentPhoto,
   },
   {
     twitterPhoto: User3Photo,
-    twitterName: 'Soham Gupta',
-    twitterAccountName: '_soham_gupta',
+    twitterName: 'Antoine Bollengier',
+    twitterAccountName: 'ATBollengier',
     twitterUrl: 'https://twitter.com/_soham_gupta/status/1647223927603884036',
     text: (
       <>
-        Although the swags are a little bit big 😅, I am nonetheless honoured to have won HackSquad
-        2022.
-        <br />
-        <br />
-        Open source with teams is always fun! 🫡
-        <br />
-        <br />
-        Thanks to <span>@nevodavid</span>, the Hacksquad 2022 challenge went really smoothly.
+        I am Proud to be a winner of HackSquad 2022
         <p className="text-with-hashtags">
           <span>@HackSquadDev</span>
+          <span>@novuhq</span>
           <span>@dailydotdev</span>
           <span>@amplication</span>
           <span>@ToolJet</span>
         </p>
+        <br />
+        Thanks to my awesome team!!!:
+        <p className="text-with-hashtags">
+          <span>@Vikash_8090_b</span>
+          <span>@royalpinto007</span>
+          <span>@AnmolSirola</span>
+          <span>@TarunH22</span>
+        </p>
       </>
     ),
+    photo: User3ContentPhoto,
   },
   {
     twitterPhoto: User4Photo,
-    twitterName: 'Srikar',
-    twitterAccountName: 'Srikarismad',
-    twitterUrl: 'https://twitter.com/Srikarismad/status/1643277099447517184',
-    text: (
-      <>
-        Glad to win these amazing swags from HackSquad 2022 🤩! . These look absolutely amazing and
-        the quality is just fab 🙌.
-        <br />
-        <br />
-        Motivates me more to participate in such events and give my best.
-        <br />
-        <br />
-        Open source with teams is always fun 🫡.
-        <p className="text-with-hashtags">
-          <span>@HackSquadDev</span>
-          <span>@novuhq</span>
-        </p>
-      </>
-    ),
-    photo: User4ContentPhoto,
-  },
-  {
-    twitterPhoto: User5Photo,
-    twitterName: 'Manish Kumar Barnwal #Hack4Bengal #NFTDay',
-    twitterAccountName: 'imanishbarnwal',
-    twitterUrl: 'https://twitter.com/imanishbarnwal/status/1640948881554014208',
-    text: (
-      <>
-        I am Proud to be a winner of HackSquad 2022 🏆 and here's my awesome winning swags 🫶
-        <br />
-        <br />
-        Thanks for sending <span>@HackSquadDev</span>
-        <span>@novuhq</span>
-        <span>@dailydotdev</span>
-        <span>@amplication</span>
-        <span>@ToolJet</span>✨
-        <br />
-        <br />
-        Also, thanks to my teammates <span>@hvbajoria</span>
-        <span>@MahantyShatanik</span>
-        <span>@ProsenjitSwarn5</span>
-        <span>@SayanMu79522282</span>
-        <p className="text-with-hashtags">
-          <span>@HackSquadDev</span>
-          <span>@novuhq</span>
-          <span>@dailydotdev</span>
-          <span>@amplication</span>
-          <span>@ToolJet</span>
-        </p>
-      </>
-    ),
-  },
-  {
-    twitterPhoto: User6Photo,
-    twitterName: 'Prosenjit Swarnakar',
-    twitterAccountName: 'ProsenjitSwarn5',
-    twitterUrl: 'https://twitter.com/ProsenjitSwarn5/status/1642069023847518208',
-    text: (
-      <>
-        I am proud to be a winner of HackSquad 2022🏆and here are my awesome swags 🫶🏽
-        <br />
-        <br /> Thanks, <span>@HackSquadDev</span>
-        <span>@novuhq</span>
-        <span>@dailydotdev</span>
-        <span>@amplication</span>
-        <span>@ToolJet</span> for sending those awesome swags. <br />
-        <br />
-        Also thanks to all my teammates <span>@MahantyShatanik</span>
-        <span>@imanishbarnwal</span>
-        <span>@SayanMu79522282</span>
-        <span>@hvbajoria</span>
-        for this.
-      </>
-    ),
-  },
-  {
-    twitterPhoto: User7Photo,
-    twitterName: 'ANURAG TIWARI',
-    twitterAccountName: 'danurag23',
-    twitterUrl: 'https://twitter.com/danurag23/status/1646013794693570561',
-    text: (
-      <>
-        Happy to share that I recieved swags from <span>@HackSquadDev</span>
-        <br />
-        <br />
-        <span>#opensource</span> Proud to be winner!!
-      </>
-    ),
-    photo: User7ContentPhoto,
-  },
-  {
-    twitterPhoto: User8Photo,
     twitterName: 'Mouri Roy',
     twitterAccountName: 'Logno_Roy',
     twitterUrl: 'https://twitter.com/Logno_Roy/status/1648053983347642368',
     text: (
       <>
-        Finally received my <span>@HackSquadDev</span> swags! Proud to be a winner and Looking
-        forward to more such events by <span>@nevodavid</span>
+        Finally received my <span className="contents">@HackSquadDev</span> swags! Proud to be a
+        winner and Looking forward to more such events by <span>@nevodavid</span>
         <p className="text-with-hashtags">
           <span>@HackSquadDev</span>
           <span>@dailydotdev</span>
@@ -196,6 +118,168 @@ const list = [
         </p>
       </>
     ),
+    photo: User4ContentPhoto,
+  },
+  {
+    twitterPhoto: User5Photo,
+    twitterName: 'Shivam Agarwal',
+    twitterAccountName: 'Shiv2002Agarwal',
+    twitterUrl: 'https://twitter.com/Srikarismad/status/1643277099447517184',
+    text: (
+      <>
+        Thanks
+        <p className="text-with-hashtags">
+          <span>@HackSquadDev</span>
+          <span>@dailydotdev</span>
+          <span>@amplication</span>
+          <span>@ToolJet</span>
+        </p>
+        for these amazing swags
+      </>
+    ),
+    photo: User5ContentPhoto,
+  },
+  {
+    twitterPhoto: User6Photo,
+    twitterName: 'Bibek Dhakal',
+    twitterAccountName: 'bibekdhkl',
+    twitterUrl: 'https://twitter.com/imanishbarnwal/status/1640948881554014208',
+    text: (
+      <>
+        Thank you <span>@HackSquadDev</span> for the amazing swags!
+        <p className="text-with-hashtags">
+          <span>#opensource</span>
+          <span>#hacksquad</span>
+          <span>#hacksquad2022</span>
+          <span>#swags</span>
+          <span>#WINNER</span>
+          <span>@HackSquadDev</span>
+          <span>@novuhq</span>
+          <span>@dailydotdev</span>
+          <span>@amplication</span>
+          <span>@ToolJet</span>
+        </p>
+      </>
+    ),
+    photo: User6ContentPhoto,
+  },
+  {
+    twitterPhoto: User7Photo,
+    twitterName: 'Priyanshu',
+    twitterAccountName: 'Kumar_Sons_off',
+    twitterUrl: 'https://twitter.com/ProsenjitSwarn5/status/1642069023847518208',
+    text: (
+      <>
+        Bit late, but finally the HackSquad22 Swags arrived. <br />
+        <br /> A great thank to <span>@nevodavid</span>& the team behind for organising such a
+        friendly & pleasant competition. <br />
+        <br /> Open-Source FTW!
+        <p className="text-with-hashtags">
+          <span>@HackSquadDev</span>
+          <span>@novuhq</span>
+          <span>@dailydotdev</span>
+          <span>@amplication</span>
+          <span>@ToolJet</span>
+        </p>
+      </>
+    ),
+    photo: User7ContentPhoto,
+  },
+  {
+    twitterPhoto: User8Photo,
+    twitterName: 'ANURAG TIWARI',
+    twitterAccountName: 'danurag23',
+    twitterUrl: 'https://twitter.com/danurag23/status/1646013794693570561',
+    text: (
+      <>
+        Happy to share that I recieved swags from <span>@HackSquadDev</span>
+        <br />
+        <br />
+        <span>#opensource</span> Proud to be winner!!
+      </>
+    ),
+    photo: User8ContentPhoto,
+  },
+  {
+    twitterPhoto: User9Photo,
+    twitterName: 'crayyy_zee for you',
+    twitterAccountName: 'crayyy_zee',
+    twitterUrl: 'https://twitter.com/Logno_Roy/status/1648053983347642368',
+    text: (
+      <>
+        Being one of the few winners of HackSquad from Pakistan is a feeling of pride within itself.
+        Thank you novu for the opportunity to participate in this event and collaborate with people
+        around the world.
+        <p className="text-with-hashtags">
+          <span>@HackSquadDev</span>
+          <span>@dailydotdev</span>
+          <span>@amplication</span>
+          <span>@ToolJet</span>
+        </p>
+      </>
+    ),
+    photo: User9ContentPhoto,
+  },
+  {
+    twitterPhoto: User10Photo,
+    twitterName: 'Prajita Adhikari',
+    twitterAccountName: 'PrajitaAdhikar4',
+    twitterUrl: 'https://twitter.com/imanishbarnwal/status/1640948881554014208',
+    text: (
+      <>
+        Thank you for the cool swags. Manifesting the win.
+        <p className="text-with-hashtags">
+          <span>@HackSquadDev</span>
+          <span>@novuhq</span>
+          <span>@dailydotdev</span>
+          <span>@amplication</span>
+          <span>@ToolJet</span>
+        </p>
+      </>
+    ),
+    photo: User10ContentPhoto,
+  },
+  {
+    twitterPhoto: User11Photo,
+    twitterName: 'Parth Shah',
+    twitterAccountName: '4gameparth',
+    twitterUrl: 'https://twitter.com/ProsenjitSwarn5/status/1642069023847518208',
+    text: (
+      <>
+        I am Proud to be a winner of HackSquad 2022
+        <p className="text-with-hashtags">
+          <span>@HackSquadDev</span>
+          <span>@novuhq</span>
+          <span>@dailydotdev</span>
+          <span>@amplication</span>
+          <span>@ToolJet</span>
+        </p>
+      </>
+    ),
+    photo: User11ContentPhoto,
+  },
+  {
+    twitterPhoto: User12Photo,
+    twitterName: 'Soham Gupta',
+    twitterAccountName: '@_soham_gupta',
+    twitterUrl: 'https://twitter.com/danurag23/status/1646013794693570561',
+    text: (
+      <>
+        Although the swags are a little bit big, I am nonetheless honoured to have won HackSquad
+        2022. <br />
+        <br /> Open source with teams is always fun! <br />
+        <br /> Thanks to <span className="contents">@nevodavid</span>, the Hacksquad 2022 challenge
+        went really smoothly.
+        <p className="text-with-hashtags">
+          <span>@HackSquadDev</span>
+          <span>@novuhq</span>
+          <span>@dailydotdev</span>
+          <span>@amplication</span>
+          <span>@ToolJet</span>
+        </p>
+      </>
+    ),
+    photo: User12ContentPhoto,
   },
 ];
 
