205e4dd65f493a8d930d88c1e736f007d7d81a2d
Raman Yasel <raman.yasel@gmail.com>
2023-09-21 14:50:43 +0300

    feat: update repositories page styles

--- src/components/pages/repositories/hero/hero.jsx
+++ src/components/pages/repositories/hero/hero.jsx
@@ -1,17 +1,10 @@
-import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React, { useCallback, useEffect, useState } from 'react';
-
-import Socials from 'components/shared/socials';
-
-import bgLeftGlitch from './images/bg-left-glitch.png';
-import bgRightGlitch from './images/bg-right-glitch.png';
-import bgTitleGlitch from './images/bg-title-glitch.png';
+import { useCallback, useEffect, useState } from 'react';
 
 import RepositoryStatus from '~/helpers/repository.status';
 
-const title = '>_Repositories';
+const title = 'Repositories';
 const leadersHeader = ['Name', 'Link', 'Status'];
 
 const Hero = () => {
@@ -30,9 +23,9 @@ const Hero = () => {
   }, [router]);
 
   return (
-    <section className="safe-paddings relative min-h-[600px]">
+    <section className="safe-paddings relative">
       <div className="container relative flex h-full flex-col items-center justify-center py-16 sm:px-0">
-        <h1 className="leading-tight font-mono text-xl font-bold uppercase lg:text-[50px] md:text-[40px] sm:px-4 xs:text-[32px]">
+        <h1 className="text-center font-titles text-60 font-semibold leading-none md:text-42">
           {title}
         </h1>
         <div className="mt-[100px] flex space-x-[20px]">
@@ -57,7 +50,10 @@ const Hero = () => {
           <div className="min-w-[716px] md:min-w-[600px] md:px-7 sm:min-w-[300px] sm:px-4">
             <div className="grid grid-cols-[50px_1fr_180px] gap-x-5 border-b border-gray-2 pb-4 sm:grid-cols-[50px_160px_40px]">
               {leadersHeader.map((header, index) => (
-                <span className="font-medium uppercase" key={index}>
+                <span
+                  className="text-18 font-medium uppercase leading-normal text-gray-1 md:text-18"
+                  key={index}
+                >
                   {header}
                 </span>
               ))}
@@ -65,8 +61,8 @@ const Hero = () => {
             <ul>
               {list.map((l) => (
                 <li
-                  key={l.id}
                   className="grid grid-cols-[50px_1fr_180px] gap-x-5 border-b border-gray-2 py-4 sm:grid-cols-[50px_160px_40px]"
+                  key={l.id}
                 >
                   <span>1</span>
                   <p className="truncate font-medium">{l.url}</p>
@@ -78,57 +74,25 @@ const Hero = () => {
             </ul>
           </div>
         </div>
-        <Link href="/" passHref legacyBehavior>
-          <a
-            className="cta-btn-animation relative mt-10 flex h-[60px] max-w-full items-center justify-center leading-none"
-            href="/"
+        <Link
+          className="cta-btn-animation relative mt-10 flex h-[60px] max-w-full items-center justify-center leading-none"
+          href="/"
+        >
+          <svg
+            className="cta-btn-animation-border xs:w-full"
+            width="268"
+            height="59"
+            viewBox="0 0 268 59"
+            fill="none"
+            aria-hidden
           >
-            <svg
-              className="cta-btn-animation-border xs:w-full"
-              width="268"
-              height="59"
-              viewBox="0 0 268 59"
-              fill="none"
-            >
-              <path d="M1 58V1H251.586L267 16.4142V58H1Z" stroke="white" strokeWidth="2" />
-            </svg>
-            <div className="absolute inset-0 flex items-center justify-center space-x-2.5">
-              <span className="text-lg sm:text-[18px]">Back to Homepage</span>
-            </div>
-          </a>
+            <path d="M1 58V1H251.586L267 16.4142V58H1Z" stroke="white" strokeWidth="2" />
+          </svg>
+          <div className="absolute inset-0 flex items-center justify-center space-x-2.5">
+            <span className="text-lg sm:text-[18px]">Back to Homepage</span>
+          </div>
         </Link>
-        <Socials className="mt-10" />
       </div>
-      <Image
-        className="absolute left-0 top-[70px] -z-20 md:top-10 sm:top-12 sm:max-w-[360px] xs:max-w-[240px]"
-        src={bgLeftGlitch}
-        width={464}
-        height={78}
-        loading="eager"
-        alt="Left Glitch image"
-        priority
-        aria-hidden
-      />
-      <Image
-        className="absolute left-0 right-0 top-0 -z-10 md:hidden"
-        src={bgTitleGlitch}
-        width={1920}
-        height={219}
-        loading="eager"
-        alt="Center Glitch image"
-        priority
-        aria-hidden
-      />
-      <Image
-        className="absolute right-0 top-9 -z-20 md:top-12 sm:top-20 sm:max-w-[360px] xs:max-w-[260px]"
-        src={bgRightGlitch}
-        width={474}
-        height={105}
-        loading="eager"
-        alt="Right Glitch image"
-        priority
-        aria-hidden
-      />
     </section>
   );
 };
--- src/pages/repositories.jsx
+++ src/pages/repositories.jsx
@@ -1,5 +1,6 @@
 import Hero from 'components/pages/repositories/hero';
 import Topfab from 'components/shared/FAB/topfab';
+import JoinUs from 'components/shared/join-us';
 import LayoutMain from 'layouts/layouts/layout-main';
 
 const Repositories = () => (
@@ -11,6 +12,7 @@ const Repositories = () => (
     isFooterBordered
   >
     <Hero />
+    <JoinUs />
     <Topfab />
   </LayoutMain>
 );
