d63facf7f637ea92edfae9e2fb88f0b3605a205b
krithikgokuls <70182240+krithikgokuls@users.noreply.github.com>
2023-09-27 13:33:55 +0530

    Create Arraylist.js

---
+++ Arraylist.js
@@ -0,0 +1,14 @@
+[
+    {
+        "name": "Alice",
+        "age": 25
+    },
+    {
+        "name": "Bob",
+        "age": 30
+    },
+    {
+        "name": "Charlie",
+        "age": 35
+    }
+]
