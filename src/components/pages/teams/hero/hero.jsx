f6c94e3af66f917905625835c6c1f0c45492e616
Nevo David <nevo@notu.co>
2023-10-03 19:42:07 +0700

    feat: crowd

--- src/components/pages/teams/hero/hero.jsx
+++ src/components/pages/teams/hero/hero.jsx
@@ -170,16 +170,18 @@ const Hero = ({ team }) => {
                         DELETED:{' '}
                       </span>
                     )}
-                    <a href={`https://github.com${  pr.url}`}>https://github.com{pr.url}</a>
+                    <a href={`https://github.com${pr.url}`}>https://github.com{pr.url}</a>
                   </p>
                   <p className="font-medium">
                     <a
-                      href={`https://analyzemyrepo.com/analyze${  pr.url}`}
+                      href={`https://analyzemyrepo.com/analyze${pr.url}`}
                       target="_blank"
                       rel="noreferrer"
-                      className="bg-white"
                     >
-                      <img src="https://uploads-ssl.webflow.com/635150609746eee5c60c4aac/635561187e62507156070c88_crowd_dev_icon.svg" />
+                      <img
+                        className="rounded-full bg-white p-[6px]"
+                        src="https://uploads-ssl.webflow.com/635150609746eee5c60c4aac/635561187e62507156070c88_crowd_dev_icon.svg"
+                      />
                     </a>
                   </p>
 
