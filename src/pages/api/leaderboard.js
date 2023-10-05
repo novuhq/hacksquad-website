55c0e290bdaee99c1d002a9640344ba39471ea17
Nevo David <nevo@notu.co>
2023-10-05 18:15:00 +0700

    feat: leaderboard change

--- src/pages/api/leaderboard.js
+++ src/pages/api/leaderboard.js
@@ -12,9 +12,23 @@ export default async function handler(req, res) {
       id: true,
       name: true,
       score: true,
+      bonus: true,
       slug: true,
     },
   });
 
-  res.status(200).json({ teams });
+  const newTeams = teams.reduce(
+    (all, current) => {
+      if ((current.score || 0) - (current.bonus || 0) === 0) {
+        all.withoutPRS.push(current);
+      } else {
+        all.withPRS.push(current);
+      }
+
+      return all;
+    },
+    { withoutPRS: [], withPRS: [] }
+  );
+
+  res.status(200).json({ teams: [...newTeams.withPRS, ...newTeams.withoutPRS] });
 }
