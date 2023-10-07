46a653943034d0e306e1167914bc7db90fec9df2
Harsh Mahajan <127186841+HarshMN2345@users.noreply.github.com>
2023-10-07 21:25:35 +0530

    Update hero.jsx
    Optimized code for conciseness and improved readability. Utilized pop() for simplicity, condensed Image elements, and used object for image properties. Removed unnecessary whitespace. Maintained original functionality.

--- src/components/pages/leaderboard/hero/hero.jsx
+++ src/components/pages/leaderboard/hero/hero.jsx
@@ -2,7 +2,6 @@ import Image from 'next/future/image';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import React, { useMemo } from 'react';
-
 import Socials from 'components/shared/socials';
 
 import bgLeftGlitch from './images/bg-left-glitch.png';
@@ -13,10 +12,7 @@ const title = '>_Leaderboard';
 const leadersHeader = ['Place', 'Name', 'Score'];
 
 const Hero = ({ teams }) => {
-  const score = useMemo(() => {
-    const tea = (teams || [])?.slice(0, 60);
-    return tea[tea.length - 1]?.score;
-  }, [teams]);
+  const score = useMemo(() => (teams?.slice(0, 60).pop()?.score || 0), [teams]);
 
   return (
     <section className="safe-paddings relative min-h-[600px]">
@@ -28,22 +24,15 @@ const Hero = ({ teams }) => {
           <div className="min-w-[716px] md:min-w-[600px] md:px-7 sm:min-w-[300px] sm:px-4">
             <div className="grid grid-cols-[120px_1fr_90px] gap-x-5 border-b border-gray-2 pb-4 sm:grid-cols-[50px_160px_40px]">
               {leadersHeader.map((header, index) => (
-                <span className="font-medium uppercase" key={index}>
-                  {header}
-                </span>
+                <span className="font-medium uppercase" key={index}>{header}</span>
               ))}
             </div>
             <ul>
               {teams.map((team, index) => (
-                <li
-                  className="grid grid-cols-[120px_1fr_90px] gap-x-5 border-b border-gray-2 py-4 sm:grid-cols-[50px_160px_40px]"
-                  key={team.slug}
-                >
+                <li className="grid grid-cols-[120px_1fr_90px] gap-x-5 border-b border-gray-2 py-4 sm:grid-cols-[50px_160px_40px]" key={team.slug}>
                   <span>{index + 1}</span>
                   <p className="truncate font-medium">
-                    <Link href={`/team/${team.slug}`}>{`${team.name} ${
-                      score <= team.score ? '👑' : ''
-                    }`}</Link>
+                    <Link href={`/team/${team.slug}`}>{`${team.name} ${score <= team.score ? '👑' : ''}`}</Link>
                   </p>
                   <span>{team.score}</span>
                 </li>
@@ -52,17 +41,8 @@ const Hero = ({ teams }) => {
           </div>
         </div>
         <Link href="/" passHref>
-          <a
-            className="cta-btn-animation relative mt-10 flex h-[60px] max-w-full items-center justify-center leading-none"
-            href="/"
-          >
-            <svg
-              className="cta-btn-animation-border xs:w-full"
-              width="268"
-              height="59"
-              viewBox="0 0 268 59"
-              fill="none"
-            >
+          <a className="cta-btn-animation relative mt-10 flex h-[60px] max-w-full items-center justify-center leading-none" href="/">
+            <svg className="cta-btn-animation-border xs:w-full" width="268" height="59" viewBox="0 0 268 59" fill="none">
               <path d="M1 58V1H251.586L267 16.4142V58H1Z" stroke="white" strokeWidth="2" />
             </svg>
             <div className="absolute inset-0 flex items-center justify-center space-x-2.5">
@@ -72,36 +52,23 @@ const Hero = ({ teams }) => {
         </Link>
         <Socials className="mt-10" />
       </div>
-      <Image
-        className="absolute top-[70px] left-0 -z-20 md:top-10 sm:top-12 sm:max-w-[360px] xs:max-w-[240px]"
-        src={bgLeftGlitch}
-        width={464}
-        height={78}
-        loading="eager"
-        alt="Left Glitch image"
-        priority
-        aria-hidden
-      />
-      <Image
-        className="absolute left-0 right-0 top-0 -z-10 md:hidden"
-        src={bgTitleGlitch}
-        width={1920}
-        height={219}
-        loading="eager"
-        alt="Center Glitch image"
-        priority
-        aria-hidden
-      />
-      <Image
-        className="absolute right-0 top-9 -z-20 md:top-12 sm:top-20 sm:max-w-[360px] xs:max-w-[260px]"
-        src={bgRightGlitch}
-        width={474}
-        height={105}
-        loading="eager"
-        alt="Right Glitch image"
-        priority
-        aria-hidden
-      />
+      {[
+        { src: bgLeftGlitch, width: 464, height: 78, top: '70px', left: '0', zIndex: '-20', maxW: '360px' },
+        { src: bgTitleGlitch, width: 1920, height: 219, top: '0', left: '0', right: '0', zIndex: '-10' },
+        { src: bgRightGlitch, width: 474, height: 105, top: '9', right: '0', zIndex: '-20', maxW: '360px' },
+      ].map(({ src, width, height, top, left, right, zIndex, maxW }, index) => (
+        <Image
+          key={index}
+          className={`absolute top-${top} left-${left} right-${right} -z-${zIndex} sm:max-w-[${maxW}] xs:max-w-[240px]`}
+          src={src}
+          width={width}
+          height={height}
+          loading="eager"
+          alt={`${src} image`}
+          priority
+          aria-hidden
+        />
+      ))}
     </section>
   );
 };
